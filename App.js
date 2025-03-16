32621dbdf400a69e45a71e0c6463e23e782e6852
Devadath Mahesh <154428727+devadathmahesh@users.noreply.github.com>
2025-03-16 19:34:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500 text-white text-3xl font-bold">
+      Tailwind is Working! 🎉
+    </div>
+  );
+}
+
+export default App;
+
